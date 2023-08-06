eac01af78add5687d298192c18b16ec122b0dd34
Kelly Scott <116105463+lladysmall@users.noreply.github.com>
2023-08-05 17:52:24 -0700

    removed the onClick call from index.html and added a on click function to  js to add a new item to the  list.  also changed the button styling to affect the id deleteButton

--- js/script.js
+++ js/script.js
@@ -1,32 +1,36 @@
 $(function newItem() {
+
     //add a new item to the list:
-    let list = $('#list');
-    let li = $('<li></li>');
-    let inputValue = $('#input').val();
-    list.append(li);
-    li.append(inputValue);
-
-    if (inputValue === '') {
-        alert("You must write something!");
-    }else {
+    $('#button').click (function() {
+        let list = $('#list');
+        let li = $('<li></li>');
+        let inputValue = $('#input').val();
+        li.addClass('list-item');
         list.append(li);
-    }
+        li.append(inputValue);
 
-    //add the delete button "X":
-    let deleteButton = $(<input type="button" id="deleteButton"/>);
-    deleteButton.append(document.createTextNode('X'));
-    li.append(deleteButton);
+        if (inputValue === '') {
+            alert("You must write something!");
+        }else {
+            list.append(li);
+        }
+
+        //add the delete button "X":
+        let deleteButton = $('<input type="button" id="deleteButton"/>');
+        deleteButton.append(document.createTextNode('X'));
+        li.append(deleteButton);
+    });
 
     //crossing out an item from the list:
-    li.on("dblclick", function() {
-        li.toggleClass("strike");
+    $('.list-item').on("dblclick", function() {
+        $('.list-item').toggleClass("strike");
     });
 
     //delete an item from the list:
-    deleteButton.on("click", function() {
-        li.addClass("delete");
+    $('#deleteButton.on').on("click", function() {
+        $('list-item').addClass("delete");
     });
 
     //change the order of the list:
-    list.sortable();
-})
+    $('ol').sortable();
+});
