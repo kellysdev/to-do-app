d592570ec374eccfd7e0ff76e4917edead12ade5
Kelly Scott <116105463+lladysmall@users.noreply.github.com>
2023-08-11 11:29:21 -0700

    change syntax of crossOut and deleteItem functions

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 $(function () {
-    let deleteButton = $('<button id="deleteButton"/>x</button>');
+    //declare global variables:
+    let deleteButton = $('<button id="deleteButton">x</button>');
+    let li = $('<li></li>');
 
     //add a new item to the list:
     $('#button').click (function newItem() {
@@ -14,18 +16,30 @@ $(function () {
         $('li').append(deleteButton);
 
         //clear the input field after adding an item to the list:
-        $('#input').val('');
+        // $('#input').val('');
     });
 
     //crossing out an item from the list:
-    $('li').on('dblclick', function crossOut() {
+    function crossOut() {
         $(this).toggleClass('strike');
-    });
+    };
+    li.on('dbclick', crossOut());
+
+    //this function worked when it was nested inside the newItem() along with the variables:
+    // $(li).on('dblclick', function crossOut() {
+    //     $(this).toggleClass('strike');
+    // });
 
     //delete an item from the list:
-    $('#deleteButton').on('click', function deleteItem() {
+    function deleteItem() {
         $(this).parent().addClass('delete');
-    });
+    };
+    $('#deleteButton').on('click', deleteItem());
+
+    //this function worked when it was nested inside the newItem() along with the variables:
+    // $('#deleteButton').on('click', function deleteItem() {
+    //     $(this).parent().addClass('delete');
+    // });
 
     //change the order of the list:
     $('ol').sortable();
